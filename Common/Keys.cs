0e55bdc8727c3f129dc0d763595ed7e76d062745
Keegan Fargher <fargherkeegan@gmail.com>
2018-12-17 22:27:02 +0200

    rewrwerwer

--- Common/Keys.cs
+++ Common/Keys.cs
@@ -4,8 +4,7 @@ namespace Common
 {
     public static class Keys
     {
-        public static string BotToken = "NDcxMDI3NTYwNTY2ODgyMzA1.DvRVCQ.sGvkODRoXcyVIICGyBt3T9Bl7Is";
-
-        public static string GiphyApi = "ku85cozWIjARSDw2rMg4jWlYHl8q75Ls";
+        public static string BotToken = "";
+        public static string GiphyApi = "";
     }
 }
